f6f821adfffbd480f49dc198243b68cfbe10cc95
Hanif Nahwan Nurhadi <hanifnurh@student.ub.ac.id>
2024-05-08 17:58:01 +0700

    Patch 1.3

--- js/script.js
+++ js/script.js
@@ -17,3 +17,26 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+function sendContact() {
+  let fullName = document.getElementById('fullName').value;
+  let email = document.getElementById('email').value;
+  let interest = document.getElementById('interest').value;
+
+  if (fullName && email && interest) {
+      alert(`${fullName}, thanks for registering.`);
+      // Save form data to localStorage
+      localStorage.setItem('fullName', fullName);
+      localStorage.setItem('email', email);
+      localStorage.setItem('interest', interest);
+
+      // Display the saved data on the webpage
+      document.getElementById('display').innerHTML = `
+          <p>Your Name: ${fullName}</p>
+          <p>Email Address: ${email}</p>
+          <p>Interested in: ${interest}</p>
+      `;
+  } else {
+      alert("There was an error. Please fill in all fields.");
+  }
+}
