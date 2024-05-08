6d8b41627b50cf02b6b93de2e58f01af5b89c0e5
Hanif Nahwan Nurhadi <hanifnurh@student.ub.ac.id>
2024-05-09 00:22:09 +0700

    Patch 1.4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navigationLinks = document.querySelector('.navigation-links');
+
+  hamburgerMenu.addEventListener('click', function () {
+      hamburgerMenu.classList.toggle('active');
+      navigationLinks.classList.toggle('active');
+  });
+});
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a').classList.add('active');
+      });
+    }
+  });
+};
+
 let slideIndex = 0;
 showSlides();
 
